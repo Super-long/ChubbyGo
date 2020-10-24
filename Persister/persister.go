85e610823d3bdebd5a0137110fe6db5be7951924
Super-long <2339824768@qq.com>
2020-10-24 22:08:00 +0800

    基础持久化已完成。文件名解析有问题;宕机重启后操作有问题;刷盘策略Always未实现

--- Persister/persister.go
+++ Persister/persister.go
@@ -5,24 +5,25 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 )
 
-// 模仿redis AOF
-const(
-	Always = iota	// 每条操作都进行刷盘 TODO 目前并不太好实现
-	Everysec		// 每秒进行一次刷盘,把raftstate和snapshot分别存到不同的文件中
-	No				// 不主动刷盘,也就隐含着我们不需要启动守护协程
+// 模仿redis AOF的命名,其实总体来看更像是RDB
+const (
+	Always   = iota // 每条操作都进行刷盘 TODO 目前并不太好实现
+	Everysec        // 每秒进行一次刷盘,把raftstate和snapshot分别存到不同的文件中
+	No              // 不主动刷盘,也就隐含着我们不需要启动守护协程
 )
 
 type Persister struct {
-	mu        sync.Mutex	// 这个锁很重要,因为raft会和守护协程都会操作这个结构体,所以需要加锁保护
+	mu        sync.Mutex // 这个锁很重要,因为raft会和守护协程都会操作这个结构体,所以需要加锁保护
 	raftstate []byte
 	snapshot  []byte
 
 	// 持久化文件名取决于用户给予了用户很高的自由度,同时方便了测试
-	SnapshotFileName	string	// 快照的持久化文件名
-	RaftstateFileName	string	// raft状态的持久化名
-	PersistenceStrategy int		// 对应着三条策略
+	SnapshotFileName    string // 快照的持久化文件名
+	RaftstateFileName   string // raft状态的持久化名
+	PersistenceStrategy int    // 对应着三条策略
 }
 
 func MakePersister() *Persister {
@@ -50,6 +51,19 @@ func (ps *Persister) ReadRaftState() []byte {
 	return ps.raftstate
 }
 
+/*
+ * @brief: 从raftstate.hdb中读取RaftState
+ * @return: 返回文件内容的[]byte
+ */
+func (ps *Persister) ReadRaftStateFromFile() []byte {
+	content, err := ReadContentFromFile(ps.RaftstateFileName)
+	if err != nil {
+		log.Println("ERROR : ", err.Error())
+		return []byte{}
+	}
+	return content
+}
+
 func (ps *Persister) RaftStateSize() int {
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
@@ -65,7 +79,7 @@ func (ps *Persister) SaveStateAndSnapshot(state []byte, snapshot []byte) {
 	ps.snapshot = snapshot
 }
 
-func (ps *Persister) SaveSnapshot(snapshot []byte){
+func (ps *Persister) SaveSnapshot(snapshot []byte) {
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
 	ps.snapshot = snapshot
@@ -83,35 +97,53 @@ func (ps *Persister) SnapshotSize() int {
 	return len(ps.snapshot)
 }
 
+/*
+ * @brief: 根据SnapshotFileName读取快照文件
+ */
+func (ps *Persister) ReadSnapshotFromFile() []byte {
+	content, err := ReadContentFromFile(ps.SnapshotFileName)
+	if err != nil {
+		log.Println("ERROR : ", err.Error())
+		return []byte{}
+	}
+	return content
+}
+
 // 考虑到要把快照和基础配置放在两个文件中,这里不把下面两个函数搞成成员函数,这样可以减少代码重复
 
 /*
  * @brief: 向文件写入数据
  * @return: 返回错误类型
- * @notes: 暂定给出存盘策略,遵循所选择策略刷入磁盘,一个守护协程负责按照策略刷入磁盘
+ * @notes: 暂定给出存盘策略,遵循所选择策略刷入磁盘,一个守护协程负责按照策略刷入磁盘;no和everysec都会进入,everysec会刷盘.
  */
-// TODO 目前需要json中选择持久化策略和存储的文件名,
+// TODO 目前需要json中选择持久化策略和存储的文件名, 记得查一下操作系统对于文件和文件夹的名称限制
 //  需要在raft开始时跑一个守护协程,负责周期刷盘
 //  并在服务器解析出文件名的时候进行读取,把数据传给persist,persist传给Raft
-func WriteContentToFile(vals []byte, outfile string) error {
+func WriteContentToFile(vals []byte, outfile string, strategy int) error {
 	file, err := os.Create(outfile)
 	if err != nil {
 		log.Println(err.Error())
 		return err
 	}
 
-	number ,errW := file.Write(vals)
-	if errW != nil || number < len(vals){
+	number, errW := file.Write(vals)
+	if errW != nil || number < len(vals) {
 		log.Println(errW.Error())
 		return errW
 	}
 
+	// TODO close失败时调用方行为怎么做？哪些情况会出现close错误
 	errC := file.Close()
-	if errC != nil{
+	if errC != nil {
 		log.Println(errC.Error())
 		return errC
 	}
-	// TODO close失败时调用方行为怎么做？
+
+	// 将调用fsync进行刷盘
+	if strategy == Everysec {
+		file.Sync()
+	}
+
 	return nil
 }
 
@@ -124,23 +156,50 @@ func ReadContentFromFile(filepath string) ([]byte, error) {
 	var NULL []byte
 	//打开文件
 	fi, err := os.Open(filepath)
-	if err != nil{
+	if err != nil {
 		log.Println(err.Error())
-		return NULL ,err
+		return NULL, err
 	}
 
 	//读取内容
 	res, errR := ioutil.ReadAll(fi)
-	if err != nil{
+	if err != nil {
 		log.Println(errR.Error())
 		return res, errR
 	}
 
 	errC := fi.Close()
-	if errC != nil{
+	if errC != nil {
 		log.Println(errC.Error())
 		return res, errC
 	}
 	// 没有错误，直接退出
 	return res, nil
+}
+
+/*
+ * @brief: 负责按照策略进行持久化,其实这个守护协程值执行everysec策略
+ */
+func PersisterDaemon(per *Persister) {
+	for {
+
+		per.mu.Lock() // 保护per.raftstate
+
+		// 不仅传一个per是为了这个函数能够复用
+		errRaftState := WriteContentToFile(per.raftstate, per.RaftstateFileName, per.PersistenceStrategy)
+		per.mu.Unlock()
+
+		if errRaftState != nil {
+			log.Println("WARNING : ", errRaftState.Error())
+		}
+		per.mu.Lock() // 保护per.snapshot
+		errSnapshot := WriteContentToFile(per.snapshot, per.SnapshotFileName, per.PersistenceStrategy)
+		per.mu.Unlock()
+
+		if errSnapshot != nil {
+			log.Println("WARNING : ", errRaftState.Error())
+		}
+
+		time.Sleep(1 * time.Second) // 每一秒持久化一次
+	}
 }
